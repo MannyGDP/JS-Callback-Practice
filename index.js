57993bb26871da03887cf02dc41768e4f0b50a06
Manny Delgado <mannygdp@gmail.com>
2023-06-20 23:51:55 -0700

    "Need Help"

--- index.js
+++ index.js
@@ -3,6 +3,59 @@ move(inventory).to(0, 0)
 
 const character = newImage('assets/green-character/static.gif')
 let direction = null;
+let x = 100;
+let y = 250;
+
+function move(element) {
+    element.style.position = 'fixed'
+
+    function moveToCoordinates(left, bottom) {
+        element.style.left = left + 'px'
+        element.style.bottom = bottom + 'px'
+    }
+
+    return {
+        to: moveToCoordinates
+    }
+}
+document.addEventListener('keydown', function(e){
+    if(e.repeat) return;
+
+    if(e.key === 'ArrowLeft'){
+        direction = 'west'
+    }
+    if(e.key === 'ArrowUp'){
+        direction = 'north'
+    }
+    if(e.key === 'ArrowRight'){
+        direction = 'east'
+    }
+    if(e.key === 'ArrowDown'){
+        direction = 'south'
+    }
+})
+function moveCharacter(){ 
+    if(direction === 'west'){
+        x = x - 1
+    }
+    if(direction === 'north'){
+        y = y + 1
+    }
+    if(direction === 'east'){
+        x = x + 1
+    }
+    if(direction === 'south'){
+        y = y - 1
+    }
+    character.style.left = x + 'px'
+    character.style.bottom = y + 'px'
+}
+
+document.addEventListener('keyup', function(e){
+    direction = null
+})
+
+
 move(character).to(100, 250)
 
 
