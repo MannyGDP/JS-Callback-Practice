81d077ab3dadc97060c27797cd88c834688b7629
Manny Delgado <mannygdp@gmail.com>
2023-06-21 00:05:19 -0700

    "Geting Somewhere"

--- index.js
+++ index.js
@@ -19,6 +19,23 @@ let direction = null;
 let x = 100;
 let y = 250;
 
+setInterval(function(){ 
+    if(direction === 'west'){
+        x = x - 1
+    }
+    if(direction === 'north'){
+        y = y + 1
+    }
+    if(direction === 'east'){
+        x = x + 1
+    }
+    if(direction === 'south'){
+        y = y - 1
+    }
+    character.style.left = x + 'px'
+    character.style.bottom = y + 'px'
+}, 1)
+
 
 document.addEventListener('keydown', function(e){
     if(e.repeat) return;
